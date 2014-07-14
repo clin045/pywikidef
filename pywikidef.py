96c315a42b0e7d36f7acca896a8ff6883b97ecdd
jerloch <jerloch@gmail.com>
2014-07-14 14:20:29 -0500

    starting

--- pywikidef.py
+++ pywikidef.py
@@ -1,1 +1,27 @@
-print("This will soon be an awesome script")
+from bs4 import BeautifulSoup
+import sys
+import io
+import urllib.request
+import argparse
+
+def getWebsite(search):
+	url = 'http://en.wikipedia.org/wiki/' + search.replace(" ", "_")
+	sys.stdout = io.TextIOWrapper(sys.stdout.buffer,'cp437','backslashreplace')
+	site = urllib.request.urlopen(url)
+	soup = BeautifulSoup(site)
+	soup.prettify()
+	print(soup)
+
+
+def main():
+	parser = argparse.ArgumentParser(prog="pywikidef", 
+							description="pywikidef")
+	parser.add_argument('--input', '-i', dest='search', required=True,
+							help='What you are looking for')
+	args = parser.parse_args()
+	search = args.search
+	getWebsite(search)
+
+
+if __name__ == '__main__':
+	main()
