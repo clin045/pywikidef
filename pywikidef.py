edd447fb4600a77cbe445fb0ae08abd598993673
jerloch <jerloch@gmail.com>
2014-07-14 15:19:34 -0500

    amount

--- pywikidef.py
+++ pywikidef.py
@@ -1,27 +1,29 @@
 from bs4 import BeautifulSoup
-import sys
-import io
 import urllib.request
 import argparse
 
-def getWebsite(search):
+def getWebsite(search, amount):
 	url = 'http://en.wikipedia.org/wiki/' + search.replace(" ", "_")
-	sys.stdout = io.TextIOWrapper(sys.stdout.buffer,'cp437','backslashreplace')
 	site = urllib.request.urlopen(url)
 	soup = BeautifulSoup(site)
 	soup.prettify()
-	print(soup)
-
+	paras = soup.find_all('p')
+	for index in range(amount):
+		try:
+			output = str(paras[index].get_text()).encode(errors='backslashreplace')
+			print(output)
+		except IndexError:
+			print("\n\nThere are no more paragraphs")
+			break
 
 def main():
-	parser = argparse.ArgumentParser(prog="pywikidef", 
-							description="pywikidef")
-	parser.add_argument('--input', '-i', dest='search', required=True,
-							help='What you are looking for')
+	parser = argparse.ArgumentParser(prog="pywikidef", description="pywikidef")
+	parser.add_argument('--input', '-i', dest='inp', required=True, help='What you are looking for')
+	parser.add_argument('--amount', '-a', dest='amount', default=1, help='Amount of paragraphs')
 	args = parser.parse_args()
-	search = args.search
-	getWebsite(search)
-
+	search = args.inp
+	amount = int(args.amount)
+	getWebsite(search, amount)
 
 if __name__ == '__main__':
 	main()
